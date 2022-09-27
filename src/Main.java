fd7da2da5981d9cf62c2b23ce8d182a18117c3ae
Mads92 <madskoie@hotmail.com>
2022-09-27 11:00:38 +0200

    All types created in Main

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,7 @@ public class Main {
     public static void main(String[] args){
             Roro r = new Roro("DK", "dk",300,400,800);
             r.loadingCargo(2,3);
-        System.out.println("Unused lane: " + r.unusedLane);
-        System.out.println("Cars loaded: " + r.carCount);
-        System.out.println("Trucks loaded: " + r.truckCount);
-        System.out.println("Capacity used: " + r.utilitylevelOfCapacity());
-        r.loadingCargo(40,0);
-        System.out.println("Unused lane: " + r.unusedLane);
-        System.out.println("Cars loaded: " + r.carCount);
-        System.out.println("Trucks loaded: " + r.truckCount);
-        System.out.println("Capacity used: " + r.utilitylevelOfCapacity());
+            Tanker t = new Tanker("dk","waa",40,80,10,10);
+        System.out.println(t.compartments[0].used);
     }
 }
